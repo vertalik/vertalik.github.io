dd39b49999dc43b25c44b976fb4e032b7b2d5a95
vertalik <kolumb.vk@gmail.com>
2020-06-29 15:13:22 +0300

    try to use smtpjs

--- form/common.js
+++ form/common.js
@@ -1,16 +1,34 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-	$("#form").submit(function() {
-		$.ajax({
-			type: "POST",
-			url: "mail.php",
-			data: $(this).serialize()
-		}).done(function() {
-			$(this).find("input").val("");
-			alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
-			$("#form").trigger("reset");
-		});
-		return false;
-	});
+// 	$("#form").submit(function() {
+// 		$.ajax({
+// 			type: "POST",
+// 			url: "mail.php",
+// 			data: $(this).serialize()
+// 		}).done(function() {
+// 			$(this).find("input").val("");
+// 			alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+// 			$("#form").trigger("reset");
+// 		});
+// 		return false;
+// 	});
 	
+// });
+
+const sendBtn = document.querySelector('.send_btn');
+sendBtn.addEventListener('click', e => {
+	e.preventDefault();
+	sendMail();
 });
+
+function sendMail() {
+	Email.send({
+		SecureToken : "2e612b14-6b7d-4642-bdbe-e5c4302ed2c1",
+		To : 'kolumb.vk@gmail.com',
+		From : "you@isp.com",
+		Subject : "This is the subject",
+		Body : "And this is the body"
+	}).then(
+	message => alert(message)
+	);
+}
