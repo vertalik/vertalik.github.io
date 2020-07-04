fc73ec68ece64ca69c8f4da83ab881f77cdf45a3
vertalik <kolumb.vk@gmail.com>
2020-07-04 15:04:55 +0300

    remove dataTable project

--- DataTable/assets/js/app.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const app = document.getElementById('app');
-const table = createHtmlElement(app, '', 'table', ['main-table']);
-const fragment = document.createDocumentFragment();
-
-window.addEventListener('DOMContentLoaded', initApp);
--- DataTable/assets/js/config.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const BASE_URL = 'http://api.odesseo.com.ua/warehouses';
-const MAX_LIMIT = 100;
-const DEFAULT_LIMIT = 10;
