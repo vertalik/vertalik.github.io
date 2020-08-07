5d2f7353ea29205f66a9815f8a16c5808c567e94
vertalik <kolumb.vk@gmail.com>
2020-08-07 14:14:49 +0300

    added new project - AppVesto

---
+++ AppVestoSite/assets/js/index.js
@@ -0,0 +1,117 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', initApp);
+
+function initApp() {
+  const mobileFilterResetBtn = document.querySelector('.mobile__filter__reset');
+  mobileFilterResetBtn.addEventListener('click', resetMobileFilters);
+
+  const viewAllVacanciesBtn = document.querySelector('.view-vacancies__btn');
+  viewAllVacanciesBtn.addEventListener('click', resetAllFilters);
+
+  const vacanciesLevelCheck = document.querySelectorAll('input[name="vac-radio"');
+  vacanciesLevelCheck.forEach(radio => radio.addEventListener('change', filteringVacancies));
+  
+  const vacanciesSelect = document.querySelectorAll('input[name="select"]');
+  vacanciesSelect.forEach(input => input.addEventListener('change', () => {
+    const isMobileFilterExist = document.querySelector('.filter__icon__trigger');
+    if (getComputedStyle(isMobileFilterExist).display === 'flex') {
+      filteringVacanciesMobileVer();
+    } else {
+      filteringVacancies();
+    }
+  }));
+
+  const mobileLevelcheck = document.querySelectorAll('.filter__check__lable');
+  mobileLevelcheck.forEach(input => input.addEventListener('click', filteringVacanciesMobileVer));
+}
+
+function getCheked(elements) {
+  for (let item of elements) {
+    if (item.checked) {
+      return item.value;
+    }
+  }
+}
+
+function filteringVacancies() {
+  const vacanciesList = document.querySelectorAll('input[name="radio-accordion"]');
+  const level = document.querySelectorAll('input[name="vac-radio"');
+  const direction = document.querySelectorAll('input[name="select"]');
+  const getSelectedDirection = getCheked(direction);
+  const getSelectedLevel = getCheked(level);
+  for (let vac of vacanciesList) {
+    if (vac.value === getSelectedLevel && vac.dataset.direction === getSelectedDirection) {
+      vac.parentElement.classList.remove('hide');
+    } else if (getSelectedLevel === 'Any level' && vac.dataset.direction === getSelectedDirection) {
+      vac.parentElement.classList.remove('hide');
+    } else if (vac.value === getSelectedLevel && getSelectedDirection === 'All Vacancies') {
+      vac.parentElement.classList.remove('hide');
+    } else if (getSelectedLevel === 'Any level' && getSelectedDirection === 'All Vacancies') {
+      vac.parentElement.classList.remove('hide');
+    } else {
+      vac.parentElement.classList.add('hide');
+    }
+  }
+}
+
+function filteringVacanciesMobileVer() {
+  const filterArr = [];
+  const mobileInputs = document.querySelectorAll('.filter__check__lable');
+  const vacanciesList = document.querySelectorAll('input[name="radio-accordion"]');
+  const countFilters = document.querySelector('.filter__count');
+  const countFiltersContainer = document.querySelector('.filter__count__container');
+  const direction = document.querySelectorAll('input[name="select"]');
+  const getSelectedDirection = getCheked(direction);
+  for (let inputs of mobileInputs) {
+    if (inputs.checked) {
+      filterArr.push(inputs.value);
+    }
+  }
+  if (filterArr.length > 0) {
+    countFilters.textContent = filterArr.length;
+    countFiltersContainer.classList.add('show__mobile__selected-filters');
+    for (let vac of vacanciesList) {
+      if (filterArr.includes(vac.value) && vac.dataset.direction === getSelectedDirection) {
+        vac.parentElement.classList.remove('hide');
+
+      } else if (getSelectedDirection === 'All Vacancies' && filterArr.includes(vac.value)) {
+        vac.parentElement.classList.remove('hide');
+
+      } else {
+        vac.parentElement.classList.add('hide');
+      }
+    }
+  } else {
+    countFiltersContainer.classList.remove('show__mobile__selected-filters');
+    for (let vac of vacanciesList) {
+      if (getSelectedDirection === 'All Vacancies') {
+        vac.parentElement.classList.remove('hide');
+      } else if (vac.dataset.direction === getSelectedDirection) {
+        vac.parentElement.classList.remove('hide');
+      } else {
+        vac.parentElement.classList.add('hide');
+      }
+    }
+  }
+}
+
+function resetAllFilters() {
+  const radioCollection = document.querySelectorAll('input[name="vac-radio"]');
+  const select = document.querySelectorAll('input[name="select"]');
+  const accordionRadioCollection = document.querySelectorAll('input[name="radio-accordion"]');
+  select[0].checked = true;
+  radioCollection[0].checked = true;
+  for (let vac of accordionRadioCollection) {
+    vac.parentElement.classList.remove('hide');
+    vac.checked = false;
+  }
+}
+
+function resetMobileFilters() {
+  const filtersInput = document.querySelectorAll('.filter__check__lable');
+  for (let input of filtersInput) {
+    input.checked = false;
+  }
+  filteringVacanciesMobileVer();
+}
