a801ac11e722da6c167bbb74b14d2a2fe10661f7
vertalik <kolumb.vk@gmail.com>
2020-08-26 15:09:30 +0300

    Added BHSens project

---
+++ BHsens/assets/php/mail.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+$recepient = "Avtokoleso121@gmail.com";
+$sitename = "BH SENS";
+
+$car = trim($_POST["car"]);
+$addInfo = trim($_POST["addInfo"]);
+$phone = trim($_POST["phone"]);
+$feedback = trim($_POST["feedback"]);
+$message = "Машина: $car \nДополнительная информация: $addInfo \nТелефон: $phone \nОбратная связь: $feedback";
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
